af81687a2061f98bc1abcfc67a6eaac4712c46c2
Stephane Karraz <stephane.karraz@epitech.eu>
2016-10-14 01:45:40 +0200

    main cleaned and secured

--- src/gomoku/main.go
+++ src/gomoku/main.go
@@ -9,16 +9,53 @@ import (
 	"github.com/gtalent/starfish/gfx"
 )
 
-// func exists(path string) (bool) {
-//     _, err := os.Stat(path)
-//     if err == nil { return true }
-//     if os.IsNotExist(err) { return false }
-//     return true
-// }
+func exists(path string) (bool) {
+    _, err := os.Stat(path)
+    if err == nil { return true }
+    if os.IsNotExist(err) { return false }
+    return true
+}
 
-func main() {
-	if !gfx.OpenDisplay(800, 600, false) {
-		return
+func checkFiles() bool {
+	if !exists("ressources/board.png") || !exists("ressources/bstone.png") ||
+		!exists("ressources/wstone.png") || !exists("ressources/LiberationSans-Bold.ttf") {
+		return false
+	}
+	return true
+}
+
+func addInput(pane *window.Drawer) {
+	quit := func() {
+		gfx.CloseDisplay()
+		os.Exit(0)
+	}
+	input.AddQuitFunc(quit)
+	input.AddKeyPressFunc(func(e input.KeyEvent) {
+		fmt.Println("Key Press! ", e.Key)
+		if e.Key == input.Key_Escape {
+			quit()
+		}
+	})
+	input.AddMousePressFunc(func(e input.MouseEvent) {
+		fmt.Println("Mouse Press!")
+		fmt.Println(e.X, e.Y, e.Button)
+		if e.Button == 1 || e.Button == 3 {
+			st := arbitre.IsStoneHere(pane, e.X, e.Y, gfx.DisplayWidth() / 55)
+			if st  != nil {
+				if e.Button == 1 {
+					st.White = true
+				} else {
+					st.White = false
+				}
+				arbitre.AppearStone(pane, e.X, e.Y, gfx.DisplayWidth() / 55)
+			}
+		}
+	})
+}
+
+func launchWindow(h, w int) bool {
+	if !gfx.OpenDisplay(h, w, false) {
+		return false
 	}
 
 	gfx.SetDisplayTitle("GoMoku")
@@ -27,44 +64,15 @@ func main() {
 	if pane.Init() {
 		gfx.AddDrawer(&pane)
 	}
+	addInput(&pane)
+	return true
+}
 
-	quit := func() {
-		gfx.CloseDisplay()
-		os.Exit(0)
+func main() {
+	if checkFiles() && launchWindow(800, 600) {
+		gfx.Main()
+	} else {
+		os.Stderr.WriteString("Couldn't launch the game\n")
 	}
-	input.AddQuitFunc(quit)
-
-	input.AddKeyPressFunc(func(e input.KeyEvent) {
-		fmt.Println("Key Press! ", e.Key)
-		if e.Key == input.Key_Escape {
-			quit()
-		}
-	})
-
-	input.AddMousePressFunc(func(e input.MouseEvent) {
-		fmt.Println("Mouse Press!")
-		fmt.Println(e.X, e.Y, e.Button)
-		if e.Button == 1 || e.Button == 3 {
-			st := arbitre.IsStoneHere(&pane, e.X, e.Y, gfx.DisplayWidth() / 55)
-			if st  != nil {
-				if e.Button == 1 {
-					st.White = true
-				} else {
-					st.White = false
-				}
-				arbitre.AppearStone(&pane, e.X, e.Y, gfx.DisplayWidth() / 55)
-			}
-		}
-		// if e.Button == 1 {
-		// 	pane.AddStone(e.X, e.Y, true)
-		// } else {
-		// 	pane.AddStone(e.X, e.Y, false)
-		// }
-	})
-
-	// input.AddMouseReleaseFunc(func(e input.MouseEvent) {
-	// 	fmt.Println("Mouse Release!")
-	// 	fmt.Println(e.X, e.Y)
-	// })
-	gfx.Main()
+	
 }
