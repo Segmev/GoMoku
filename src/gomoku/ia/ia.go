f9bce4b4241fe0d28f0a5795cc3df0c447ab75a0
terrea_l <leo.terreau@epitech.eu>
2016-12-09 15:52:43 +0000

    push

--- src/gomoku/ia/ia.go
+++ src/gomoku/ia/ia.go
@@ -25,7 +25,7 @@ func AddSon(tree *NodeTree, x int, y int, true_color, color, rule1, rule2 bool) 
 	son.y = y
 	son.father = tree
 	if color == true_color {
-		son.val = tree.val + 1
+		son.val = tree.val + 2
 	} else {
 		son.val = tree.val - 1
 	}
@@ -51,34 +51,37 @@ func Seek(carte [363]uint64, t_color bool, deep int, rule1, rule2 bool) (int, in
 	check := true
 	for curr != nil {
 		check = true
-		if bmap.GetValStones(&carte, x, y, bmap.MO)+bmap.GetValStones(&carte, x, y, bmap.MT) != 0 && cpt < deep { // Add a call to ApplyRules
+		if bmap.GetValStones(&carte, x, y, bmap.MO)+bmap.GetValStones(&carte, x, y, bmap.MT) != 0 && cpt < deep {
 			check = AddSon(curr, x, y, t_color, color, rule1, rule2)
-			curr = curr.sons[len(curr.sons)-1]
-			cpt = cpt + 1
-			color = !color
+			if !check {
+				curr = curr.sons[len(curr.sons)-1]
+				cpt = cpt + 1
+				color = !color
+			}
 		}
 		if (result == nil) || (result.val < curr.val && cpt < best_cpt) {
 			if curr != &racine {
 				result = curr
 			}
 		}
-	}
-	if check {
-		if x == 18 {
-			x = 0
-			if y == 18 {
-				x = curr.x
-				y = curr.y
-				curr = curr.father
-				cpt = cpt + 1
-				color = !color
+		if check {
+			if x == 18 {
+				if y == 18 {
+					x = curr.x
+					y = curr.y
+					curr = curr.father
+					cpt = cpt + 1
+					color = !color
+				} else {
+					x = 0
+					y = y + 1
+				}
 			} else {
-				y = y + 1
+				x = x + 1
 			}
-		} else {
-			x = x + 1
 		}
 	}
+	println(result)
 	if result == nil {
 		return 9, 9
 	}
